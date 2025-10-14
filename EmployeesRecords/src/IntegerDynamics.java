b5b30f92e21ff0b71792288e67f7c515655953c5
Chuong T. N. Mayian <nyangchuongtiutiu@gmail.com>
2025-10-14 23:46:27 +0000

    Add files via upload

---
+++ EmployeesRecords/src/IntegerDynamics.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class IntegerDynamics {
+    private int minimumAcceptedValue;
+    private int maximumAcceptedValue;
+    private String promptString;
+    public  IntegerDynamics(int minimumAcceptedValue, int maximumAcceptedValue, String promptString){
+        this.maximumAcceptedValue = maximumAcceptedValue;
+        this.minimumAcceptedValue = minimumAcceptedValue;
+        this.promptString = promptString;
+    }
+    public  void setInputParameters(int minimumAcceptedValue, int maximumAcceptedValue, String promptString){
+        this.maximumAcceptedValue = maximumAcceptedValue;
+        this.minimumAcceptedValue = minimumAcceptedValue;
+        this.promptString = promptString;
+    }
+    public int getValue(){
+        Scanner sc = new Scanner(System.in);
+        int value =
+        System.out.println("Enter the number ");
+    }
+}
